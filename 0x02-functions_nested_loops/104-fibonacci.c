9ccca54248c9eb15b5dc10d348f45cf1a07edaca
Francis Macharia <francismureithim@gmail.com>
2023-08-16 02:42:32 +0300

    fib

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,20 +7,23 @@
  */
 int main(void)
 {
-	unsigned long fib1 = 1, fib2 = 2, nextFib;
-	int count;
+    unsigned long fib1 = 1, fib2 = 2, nextFib;
+    int count;
+    unsigned long sum = 2; // Start with sum as 2 (since the sequence starts with 1 and 2)
 
-	printf("%lu, %lu", fib1, fib2);
+    printf("%lu, %lu", fib1, fib2);
 
-	for (count = 2; count < 98; count++)
-	{
-		nextFib = fib1 + fib2;
-		printf(", %lu", nextFib);
-		fib1 = fib2;
-		fib2 = nextFib;
-	}
+    for (count = 2; count < 98; count++)
+    {
+        nextFib = fib1 + fib2;
+        if (nextFib % 2 == 0) // Check if the nextFib is even
+            sum += nextFib;   // Add even Fibonacci numbers to the sum
+        printf(", %lu", nextFib);
+        fib1 = fib2;
+        fib2 = nextFib;
+    }
 
-	printf("\n");
+    printf("\nSum of even-valued terms: %lu\n", sum);
 
-	return (0);
+    return (0);
 }
