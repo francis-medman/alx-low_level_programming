82070b1364f82239f37d9419afdc8f0992db79d1
Francis Macharia <francismureithim@gmail.com>
2023-08-16 03:11:06 +0300

    correct

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,11 +9,11 @@
  */
 void print_alphabet(void)
 {
-    char alphabet;
+	char alphabet;
 
-    for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-    {
-        _putchar(alphabet);
-    }
-    _putchar('\n');
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
 }
