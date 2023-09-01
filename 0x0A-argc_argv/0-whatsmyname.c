158ef1bcdf0638eb3a08ce257f7f269d985d6c46
Francis Macharia <francismureithim@gmail.com>
2023-09-01 19:56:44 +0300

    retake

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,17 @@
+#include "main.h"
 #include <stdio.h>
 
+/**
+  * main - entry
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero. success execution
+  */
 int main(int argc, char *argv[])
 {
-	if (argc > 0)
-	{
-		printf("%s\n", argv[0]);
-		return 0;
-	}
-	return 1;
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
