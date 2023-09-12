f7cd60e08110313eb46529f979bb1f32a6006a10
Francis Macharia <francismureithim@gmail.com>
2023-09-12 22:37:35 +0300

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - Dog attributes
+  * @name: The dog's name
+  * @age: The dog's age
+  * @owner: The owner of the dog
+  *
+  * Description: The attributes of the dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
